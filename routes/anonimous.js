6946131d3115237300be097e0d5a02ca2c0354e0
LeeYeonhee-00 <dldusgml0929@gachon.ac.kr>
2023-08-27 03:20:35 +0900

    add chat, tag

--- routes/anonimous.js
+++ routes/anonimous.js
@@ -1,22 +1,21 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const upload = require('../middlewares/multer.middleware');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const upload = require("../middlewares/multer.middleware");
 // var requestIp = require('request-ip');
-const Room = require('../schemas/room');
-const Chat = require('../schemas/chat');
-const logger = require('../logger');
+const Room = require("../schemas/room");
+const Chat = require("../schemas/chat");
+const logger = require("../logger");
 const router = express.Router();
 
-
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     logger.info(`익명 채팅하기 페이지 요청 - ${res.locals.ip}`);
     const rooms = await Room.find({});
-    console.log('req.session ID: ', req.session);
+    console.log("req.session ID: ", req.session);
 
-    res.render('chat-main', { rooms, title: '익명 채팅방' });
+    res.render("chat-main", { rooms, title: "익명 채팅방" });
     logger.info(`익명 채팅하기 페이지 랜더링 완료 - ${res.locals.ip}`);
   } catch (error) {
     console.error(error);
@@ -25,14 +24,14 @@ router.get('/', async (req, res, next) => {
 });
 
 // 채팅방을 생성하기 위해 설정을 입력받는 부분으로.
-router.get('/room', (req, res) => {
+router.get("/room", (req, res) => {
   logger.info(`채팅방 생성 페이지 요청 시작 - ${res.locals.ip}`);
-  res.render('chat-room', { title: '익명 채팅방 생성' });
+  res.render("chat-room", { title: "익명 채팅방 생성" });
   logger.info(`채팅방 생성 페이지 랜더링 완료 - ${res.locals.ip}`);
 });
 
 // 원하는 채팅방 설정 입력한 것을 가지고 새로운 채팅방을 생성하는 부분.
-router.post('/room', async (req, res, next) => {
+router.post("/room", async (req, res, next) => {
   try {
     logger.info(`채팅방 생성 요청 시작 - ${res.locals.ip}`);
     const newRoom = await Room.create({
@@ -43,49 +42,52 @@ router.post('/room', async (req, res, next) => {
     });
 
     // app.set('io', io)로 웹소켓 모듈을 등록해두었기 때문에, 이 코드로 라우터에서 웹 소켓을 이용할 수 있음
-    const io = req.app.get('io');
+    const io = req.app.get("io");
 
-    io.of('/room').emit('newRoom', newRoom);
+    io.of("/room").emit("newRoom", newRoom);
     logger.info(`채팅방 생성 완료 - ${res.locals.ip}`);
-    res.redirect(`/anonimous/room/${newRoom._id}?password=${req.body.password}`);
+    res.redirect(
+      `/anonimous/room/${newRoom._id}?password=${req.body.password}`
+    );
   } catch (error) {
     logger.info(`채팅방 생성 실패 - ${res.locals.ip}`);
     next(error);
   }
 });
 
-
 // req.params.id 에 해당하는 방에 접속하는 라우터
-router.get('/room/:id', async (req, res, next) => {
+router.get("/room/:id", async (req, res, next) => {
   try {
     logger.info(`채팅방 접속 요청 시작 - ${res.locals.ip}`);
     // 방 접속 시 기존 채팅 내역을 불러옴.
     const room = await Room.findOne({ _id: req.params.id });
-    const io = req.app.get('io');
+    const io = req.app.get("io");
 
     if (!room) {
-      return res.redirect('/anonimous/?error=존재하지 않는 방입니다.');
+      return res.redirect("/anonimous/?error=존재하지 않는 방입니다.");
     }
     if (room.password && room.password !== req.query.password) {
-      return res.redirect('/anonimous/?error=비밀번호가 틀렸습니다.');
+      return res.redirect("/anonimous/?error=비밀번호가 틀렸습니다.");
     }
 
-    const { rooms } = io.of('/chat').adapter; // 이 adapter 는 뭐지??
-    if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
-      return res.redirect('/anonimous/?error=허용 인원이 초과하였습니다.');
+    const { rooms } = io.of("/chat").adapter; // 이 adapter 는 뭐지??
+    if (
+      rooms &&
+      rooms[req.params.id] &&
+      room.max <= rooms[req.params.id].length
+    ) {
+      return res.redirect("/anonimous/?error=허용 인원이 초과하였습니다.");
     }
-
 
     // 방에 접속할 때는 DB로부터 채팅 내역을 가져옴.
-    const chats = await Chat.find({ room: room._id }).sort('createdAt');
+    const chats = await Chat.find({ room: room._id }).sort("createdAt");
     logger.info(`채팅방 접속 요청 완료 - ${res.locals.ip}`);
-    return res.render('chat-chat', {
+    return res.render("chat-chat", {
       room,
       title: room.title,
-      chats,  // 채팅 내역
-      user: req.session.color,  // user를 익명처리 했기 때문에, req.session.id 가 아니라 req.session.color 로 했음.
+      chats, // 채팅 내역
+      user: req.session.color, // user를 익명처리 했기 때문에, req.session.id 가 아니라 req.session.color 로 했음.
     });
-
   } catch (error) {
     logger.info(`채팅방 접속 요청 실패 - ${res.locals.ip}`);
     return next(error);
@@ -93,13 +95,13 @@ router.get('/room/:id', async (req, res, next) => {
 });
 
 // 채팅방 삭제
-router.delete('/room/:id', async (req, res, next) => {
+router.delete("/room/:id", async (req, res, next) => {
   try {
     await Room.remove({ _id: req.params.id });
     await Chat.remove({ room: req.params.id });
-    res.send('ok');
+    res.send("ok");
     setTimeout(() => {
-      req.app.get('io').of('/room').emit('removeRoom', req.params.id);
+      req.app.get("io").of("/room").emit("removeRoom", req.params.id);
     }, 2000);
   } catch (error) {
     next(error);
@@ -108,10 +110,13 @@ router.delete('/room/:id', async (req, res, next) => {
 
 // 보낸 채팅을 데이터베이스에 저장 후 io.of('/chat').to(방 아이디).emit()으로 같은 방에 들어 있는 소켓들에게 메세지 데이터 전송하는 라우터.
 // 채팅을 보낼 때마다 채팅 내용이 이 라우터로 전송되고, 라우터에서 다시 웹소켓으로 메세지를 보낸다.
-router.post('/room/:id/chat', async (req, res, next) => {
+router.post("/room/:id/chat", async (req, res, next) => {
   try {
-    logger.info(`텍스트 채팅 전송 요청 시작 - ${res.locals.ip}`);
-    const chat = await Chat.create({  // 채팅 생성
+    logger.info(
+      `텍스트 채팅 전송 요청 시작 - ${res.locals.ip} ${req.body.chat}`
+    );
+    const chat = await Chat.create({
+      // 채팅 생성
       room: req.params.id,
       user: req.session.color,
       chat: req.body.chat,
@@ -120,9 +125,9 @@ router.post('/room/:id/chat', async (req, res, next) => {
     // req.app.get('io')을 통해 라우터에서 웹소켓으로 메세지를 보낼 수 있음.
 
     // io.of('/chat').to(방 아이디).emit() 으로 같은 방에 들어있는 소켓들에게 메세지 전송
-    req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat); // 보낸 채팅 메세지 내용을 웹소켓으로 전송.
+    req.app.get("io").of("/chat").to(req.params.id).emit("chat", chat); // 보낸 채팅 메세지 내용을 웹소켓으로 전송.
     logger.info(`텍스트 채팅 전송 요청 완료 - ${res.locals.ip}`);
-    res.send('ok');
+    res.send("ok");
   } catch {
     logger.info(`텍스트 채팅 전송 요청 실패 - ${res.locals.ip}`);
     next(error);
@@ -149,23 +154,21 @@ router.post('/room/:id/chat', async (req, res, next) => {
 //   limits: { fileSize: 5 * 1024 * 1024 },
 // });
 
-
-router.post('/room/:id/gif', upload.single('gif'), async (req, res, next) => {
+router.post("/room/:id/gif", upload.single("gif"), async (req, res, next) => {
   try {
     logger.info(`이미지 채팅 전송 요청 시작 - ${res.locals.ip}`);
     const chat = await Chat.create({
       room: req.params.id,
       user: req.session.color,
-      gif: req.file.location
+      gif: req.file.location,
     });
-    req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat);
+    req.app.get("io").of("/chat").to(req.params.id).emit("chat", chat);
     logger.info(`이미지 채팅 전송 요청 완료 - ${res.locals.ip}`);
-    res.send('ok');
+    res.send("ok");
   } catch (error) {
     logger.info(`이미지 채팅 전송 요청 실패 - ${res.locals.ip}`);
     next(error);
   }
 });
-
 
 module.exports = router;
--- routes/page.js
+++ routes/page.js
@@ -1,42 +1,44 @@
-const express = require('express');
-const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
-const { Post, User, Hashtag } = require('../models');
-const logger = require('../logger');
+const express = require("express");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
+const { Post, User, Hashtag } = require("../models");
+const logger = require("../logger");
 const router = express.Router();
 
 router.use((req, res, next) => {
   res.locals.user = req.user;
   res.locals.followerCount = req.user ? req.user.Followers.length : 0;
   res.locals.followingCount = req.user ? req.user.Followings.length : 0;
-  res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
+  res.locals.followerIdList = req.user
+    ? req.user.Followings.map((f) => f.id)
+    : [];
   next();
 });
 
-router.get('/profile', isLoggedIn, (req, res) => {
+router.get("/profile", isLoggedIn, (req, res) => {
   logger.info(`내 프로필 페이지 조회 요청 시작 - ${res.locals.ip}`);
-  res.render('profile', { title: '내 정보 - Elastic-SNS' });
+  res.render("profile", { title: "내 정보 - Elastic-SNS" });
   logger.info(`내 프로필 페이지 랜더링 완료 - ${res.locals.ip}`);
 });
 
-router.get('/join', isNotLoggedIn, (req, res) => {
+router.get("/join", isNotLoggedIn, (req, res) => {
   logger.info(`내 회원가입 페이지 조회 요청 시작 - ${res.locals.ip}`);
-  res.render('join', { title: '회원가입 - Elastic-SNS' });
+  res.render("join", { title: "회원가입 - Elastic-SNS" });
   logger.info(`내 회원가입 페이지 랜더링 조회 완료 - ${res.locals.ip}`);
 });
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     logger.info(`메인 페이지 접속 요청 시작 - ${res.locals.ip}`);
     const posts = await Post.findAll({
       include: {
         model: User,
-        attributes: ['id', 'nick'],
+        attributes: ["id", "nick"],
       },
-      order: [['createdAt', 'DESC']],
+      order: [["createdAt", "DESC"]],
     });
     logger.info(`메인 페이지 접속 요청 완료 - ${res.locals.ip}`);
-    res.render('main', {
-      title: 'Elastic-SNS',
+    res.render("main", {
+      title: "Elastic-SNS",
       twits: posts,
     });
   } catch (err) {
@@ -45,11 +47,11 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.get('/hashtag', async (req, res, next) => {
-  logger.info(`해시테그 검색 요청 시작 - ${res.locals.ip}`);
+router.get("/hashtag", async (req, res, next) => {
+  logger.info(`해시 태그 검색 요청 시작 - ${res.locals.ip} ${query}`);
   const query = req.query.hashtag;
   if (!query) {
-    return res.redirect('/');
+    return res.redirect("/");
   }
   try {
     const hashtag = await Hashtag.findOne({ where: { title: query } });
@@ -57,13 +59,13 @@ router.get('/hashtag', async (req, res, next) => {
     if (hashtag) {
       posts = await hashtag.getPosts({ include: [{ model: User }] });
     }
-    logger.info(`해시테그 검색 요청 완료 - ${res.locals.ip}`);
-    return res.render('main', {
+    logger.info(`해시 태그 검색 요청 완료 - ${res.locals.ip}`);
+    return res.render("main", {
       title: `${query} | Elastic-SNS`,
       twits: posts,
     });
   } catch (error) {
-    logger.info(`해시테그 검색 요청 실패 - ${res.locals.ip}`);
+    logger.info(`해시 태그 검색 요청 실패 - ${res.locals.ip}`);
     return next(error);
   }
 });
